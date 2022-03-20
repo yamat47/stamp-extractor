380d07e5fd4101d801b3a36f651939550f5324a5
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-03-20 22:45:58 +0900

    画像の背景を捨てる処理を作った。

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
+require 'rmagick'
+
 class ResultsController < ApplicationController
   def create
-    raise result_params.inspect
+    image = Magick::Image.from_blob(result_params[:image].read)[0]
+
+    frame = image.clone
+                 .threshold(Magick::QuantumRange * 0.6)
+                 .transparent('white', alpha: Magick::TransparentAlpha)
+
+    frame.format = 'png'
+
+    after = image.clone
+                 .composite(frame, Magick::CenterGravity, Magick::DstInCompositeOp)
+                 .transparent('black', alpha: Magick::TransparentAlpha)
+
+    after.format = 'png'
+
+    File.open('tmp/output.png', 'wb') { |f| f.write(after.to_blob) }
   end
 
   def show; end
