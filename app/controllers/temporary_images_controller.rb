2649bd9f6d663fa953089421b127fc70eb0fb4ef
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-03-20 23:57:18 +0900

    SVGを生成しつつ結果画面に表示する仕組みを作った。

--- app/controllers/temporary_images_controller.rb
+++ app/controllers/temporary_images_controller.rb
@@ -2,8 +2,20 @@
 
 class TemporaryImagesController < ApplicationController
   def show
-    image = File.open("tmp/#{params[:id]}.png", 'rb')
+    respond_to do |format|
+      format.png do
+        image = File.read("tmp/#{params[:id]}.png")
 
-    send_data image.read, type: 'image/png', disposition: 'inline'
+        send_data image, type: 'image/png', disposition: 'inline'
+      end
+
+      format.svg do
+        image = File.open("tmp/#{params[:id]}.svg", 'rb')
+        document = Nokogiri::HTML::DocumentFragment.parse image.read
+        svg = document.at_css "svg"
+
+        send_data svg, type: 'image/svg+xml', disposition: 'inline'
+      end
+    end
   end
 end
